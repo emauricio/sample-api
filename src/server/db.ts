1c0e5bf4fdb40c31c4e464d0813c487d9f44feaf
Efren Mauricio <efren.mauricio@gmail.com>
2019-03-27 00:24:30 +0900

    fixes mongoose connection and docker-compose (timing issue)

--- src/server/db.ts
+++ src/server/db.ts
@@ -4,17 +4,33 @@ import Logger from './utils/logger';
 const logger = Logger(__filename);
 
 function setUpConnection() {
-    // TODO: get form config file
-    const db = 'mongodb://root:rootPass@localhost:27017/sample?authSource=admin';
+    // TODO: get form config file or env
+    const mongoPath = process.env.MONGO_PATH || 'localhost:27017/sample';
+    const mongoUri = `mongodb://${mongoPath}?authSource=admin`;
     const mongooseOptions = {
-        useNewUrlParser: true
+        dbName: 'sample',
+        keepAlive: true,
+        keepAliveInitialDelay: 300000,
+        pass: 'rootPass',
+        useNewUrlParser: true,
+        user: 'root'
     };
 
-    mongoose.connect(db, mongooseOptions).then(() => {
-        logger.debug('Connected to mongodb');
-    }).catch(e => {
-        logger.error('Error connecting to database: ', e);
-        process.exit(1);
+    mongoose
+        .connect(mongoUri, mongooseOptions)
+        .then(() => {
+            logger.debug('Connected to mongodb');
+        })
+        .catch(e => {
+            logger.error('Error connecting to database: ', e);
+            process.exit(1);
+        });
+
+    process.on('SIGINT', () => {
+        mongoose.connection.close(() => {
+            logger.error('App terminated, closing mongo connections');
+            process.exit(0);
+        });
     });
 }
 
